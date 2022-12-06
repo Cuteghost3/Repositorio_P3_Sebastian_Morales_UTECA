9b6ecf28887ba0122ad8e1f86d094e6e76f02fb7
Cuteghost3 <113481723+Cuteghost3@users.noreply.github.com>
2022-12-06 12:07:30 -0600

    Arte del Juego
    No pude avanzar mas que en el arte

--- Dead Knight/Assets/Scrips/PlayerControl.cs
+++ Dead Knight/Assets/Scrips/PlayerControl.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float speed = 2f;
+    private Vector2 Target;
+    public Vector3 Offset;
 
-    private Vector2 target;
-
+    private void Aweke()
+    {
+        Target = new Vector2(-4, Offset.y);
+    }
     void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         if (Input.GetMouseButtonDown(0))
         {
-            target = new Vector2(mousePos.x, 0);
+            Target = new Vector2(mousePos.x, Offset.y);
         }
 
-        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
-        }
+        transform.position = Vector2.MoveTowards(transform.position, Target, speed * Time.deltaTime);
         
     }
 }
